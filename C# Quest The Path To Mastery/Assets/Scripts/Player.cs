dbbcb232e428132b0b21bf0e8ddffefbb7b15631
IgrUnity <97157951+IgrUnity@users.noreply.github.com>
2024-06-11 00:18:17 +0500

    Приседание персонажа добавлено в игру

--- C# Quest The Path To Mastery/Assets/Scripts/Player.cs
+++ C# Quest The Path To Mastery/Assets/Scripts/Player.cs
@@ -7,19 +7,25 @@ public class Player : MonoBehaviour
     [SerializeField] private float speedWalk;
     [SerializeField] private float gravity;
     [SerializeField] private float jumpForce;
+    [SerializeField] private float speedRun;
+    [SerializeField] private float speedSit;
 
     private CharacterController characterController;
     private Vector3 walkDirection;
     private Vector3 velocity;
+    private float speed;    
 
     private void Start()
     {
+        speed = speedWalk;
         characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
         Jump(characterController.isGrounded && Input.GetKey(KeyCode.Space));
+        Run(Input.GetKey(KeyCode.LeftControl));
+        Sit(Input.GetKey(KeyCode.LeftShift));
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         walkDirection = transform.right * x + transform.forward * z;
@@ -50,5 +56,15 @@ public class Player : MonoBehaviour
     {
         if(canJump)
             velocity.y = jumpForce;
+    }
+
+    private void Run(bool canRun)
+    {
+        speedWalk = canRun ? speedRun : speed;
+    }
+
+    private void Sit(bool canSit)
+    {
+        characterController.height = canSit ? 1f : 2f;
     }
 }
