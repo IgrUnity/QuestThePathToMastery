3f9eb5f05d712ebdec622ab2bdb497b1a5a651af
IgrUnity <97157951+IgrUnity@users.noreply.github.com>
2024-06-11 01:08:41 +0500

    Доделал персонажа Сделал отображение параметров при нажитии на f12

--- C# Quest The Path To Mastery/Assets/Scripts/Player.cs
+++ C# Quest The Path To Mastery/Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
@@ -9,26 +10,44 @@ public class Player : MonoBehaviour
     [SerializeField] private float jumpForce;
     [SerializeField] private float speedRun;
     [SerializeField] private float speedSit;
+    [SerializeField] private GameObject parameters;
+    [SerializeField] private TextMeshProUGUI speedDisplayText;
 
     private CharacterController characterController;
     private Vector3 walkDirection;
     private Vector3 velocity;
-    private float speed;    
+    private float currentSpeed;
+    private bool isRunning;
+    private bool isSitting;
 
     private void Start()
     {
-        speed = speedWalk;
+        currentSpeed = speedWalk;
         characterController = GetComponent<CharacterController>();
     }
 
     private void Update()
     {
+        if(Input.GetKeyUp(KeyCode.F12) && parameters.activeInHierarchy == true)
+        {
+            parameters.SetActive(false);
+        }
+        else if (Input.GetKeyUp(KeyCode.F12) && parameters.activeInHierarchy == false)
+        {
+            parameters.SetActive(true);
+        }
         Jump(characterController.isGrounded && Input.GetKey(KeyCode.Space));
         Run(Input.GetKey(KeyCode.LeftControl));
         Sit(Input.GetKey(KeyCode.LeftShift));
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
         walkDirection = transform.right * x + transform.forward * z;
+
+        // Update the speed display
+        if (speedDisplayText != null)
+        {
+            speedDisplayText.text = $"Speed: {currentSpeed:F2}";
+        }
     }
 
     private void FixedUpdate()
@@ -39,32 +58,53 @@ public class Player : MonoBehaviour
 
     private void Walk(Vector3 direction)
     {
-        characterController.Move(direction * speedWalk * Time.fixedDeltaTime);
+        characterController.Move(direction * currentSpeed * Time.fixedDeltaTime);
     }
 
     private void DoGravity(bool isGrounded)
     {
-        if(isGrounded && velocity.y < 0)
+        if (isGrounded && velocity.y < 0)
         {
             velocity.y = -1f;
-        }            
+        }
         velocity.y -= gravity * Time.fixedDeltaTime;
-        characterController.Move(velocity * Time.fixedDeltaTime);        
+        characterController.Move(velocity * Time.fixedDeltaTime);
     }
 
     private void Jump(bool canJump)
     {
-        if(canJump)
+        if (canJump)
             velocity.y = jumpForce;
     }
 
     private void Run(bool canRun)
     {
-        speedWalk = canRun ? speedRun : speed;
+        if (canRun && !isSitting)
+        {
+            currentSpeed = speedRun;
+        }
+        else
+        {
+            currentSpeed = speedWalk;
+        }
+        isRunning = canRun;
     }
 
     private void Sit(bool canSit)
     {
         characterController.height = canSit ? 1f : 2f;
+        isSitting = canSit;
+        if (canSit)
+        {
+            currentSpeed = speedSit;
+        }
+        else if (isRunning)
+        {
+            currentSpeed = speedRun;
+        }
+        else
+        {
+            currentSpeed = speedWalk;
+        }
     }
 }
